5c4a0aa88894662c96d9290ccaf9cef44a768989
adzy404 <adam.isitmez@hotmail.com>
2024-03-19 23:05:40 +1100

    update contents datatype

--- models/Post.js
+++ models/Post.js
@@ -16,7 +16,7 @@ Post.init(
             allowNull: false,
         },
         contents: {
-            type: DataTypes.STRING,
+            type: DataTypes.TEXT('long'),
             allowNull: false,
         },
         user_id: {
